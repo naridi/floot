705aadebc5efd1612198d7b16aca493fc9a18783
Nibal Aridi <nibal.aridi@gmail.com>
2017-01-03 18:29:46 +0000

    S3 uploading created

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,7 @@
+# Configurations for S3 uploading
+# only use Amazon S3 on production servers
+
+unless Rails.env.development?
+    Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+end
