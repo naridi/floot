2376609234e01c57eb4430168d3d56e5cfa77b67
Nibal Aridi <nibal.aridi@gmail.com>
2017-01-04 00:28:37 +0000

    Email pushing created.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,15 @@ Rails.application.configure do
    }
   }
   
+  # For SendGrid emails
+  config.action_mailer.default_url_options = { :host => 'floot.cc' }
+  ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
+  
 end
