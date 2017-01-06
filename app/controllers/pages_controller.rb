ebd62b4e0360c97c6d135303dfd965ff9dc428a8
Nibal Aridi <nibal.aridi@gmail.com>
2017-01-06 04:00:45 +0000

    Switched floots from 24 to 16 hour showings.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,12 @@ class PagesController < ApplicationController
   end
   
   def home
-    @posts = Post.where('created_at >= :one_days_ago', one_days_ago: Time.now - 1.day)
+    @posts = Post.where('created_at >= :one_days_ago', one_days_ago: Time.now - 16.hours)
     @newPost = Post.new
   end
 
   def explore
-    @posts = Post.where('created_at >= :one_days_ago', one_days_ago: Time.now - 1.day)
+    @posts = Post.where('created_at >= :one_days_ago', one_days_ago: Time.now - 16.hours)
   end
 
   def privacy
@@ -19,7 +19,7 @@ class PagesController < ApplicationController
     if (User.find_by_username(params[:id]))
       @username = params[:id]
       @users = User.all.where("id = ?", User.find_by_username(params[:id]).id)
-      @posts = Post.where(user_id: User.find_by_username(params[:id]).id).where('created_at > ?', 1.days.ago)
+      @posts = Post.where(user_id: User.find_by_username(params[:id]).id).where('created_at > ?', 16.hours.ago)
      else
       redirect_to root_path, notice: 'User not found.'
     end
