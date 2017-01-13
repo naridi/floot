9041fd0ce501843c61cc03e93fb8be846d5662b2
Nibal Aridi <nibal.aridi@gmail.com>
2017-01-13 22:13:18 +0000

    Logo, avatar, and icon updates.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ActiveRecord::Base
         
         attr_accessor :current_password # check current pasword only if user is trying to change new password in edit.html.erb
         
-        has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+        has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/default_0.png"
         validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
